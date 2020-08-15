5d10657e1842e415031178c7b851bebf9b495509
xjm <1692101110@qq.com>
2020-08-15 08:13:15 +0800

    完成底部公共模块

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 import './lib/jquery-2.1.4.js';
-import header from './modules/index_mod/header.js';
-header();
+$(function () {
+    /*公共部分：头部导航栏*/
+    $('#header').load("./header.html",function(){});
+
+    /*公共部分：加载尾部*/
+    $('#footer').load("./footer.html",function(){
+        $('.footer_pic_new').css('display','none');
+        $('.public_footer_new').css('display','none');
+        
+        $('.footer_copyright_none').css('display','none');
+
+    });
+    /*执行广告大图动画*/
+    setTimeout(function () {
+        $('.ad_bar_top').animate({ height: 200 }, 'slow')
+    }, 3000)
+})
+
+
+$(document).ready(function () {
+
+
+})
+
+$('.tools').css('display', 'none');
--- js/modules/index_mod/header.js
+++ js/modules/index_mod/header.js
@@ -1,36 +1,35 @@
-export default function () {
-
-    //完成头部导航条hover效果
-    $('.menu_box').mouseenter(function () {
-        $(this).children().eq(0)[0].classList.add('hover');
-        $(this).children().eq(1).css('display', 'block');
-    })
-    $('.menu_box').mouseleave(function () {
-        $(this).children().eq(0)[0].classList.remove('hover');
-        $(this).children().eq(1).css('display', 'none');
-    })
-
-
-    // 完成加载省份名称的加载渲染
-    $.get('../data/address.json', '', function (data) {
-
-        $.each(data, function (index, item) {
-            var newLi = document.createElement('li');
-            newLi.innerHTML ='<a href="">'+item.province+'</a>';
-            $('.address-box').append(newLi);
-        })
-    }, 'json')
-
-    // 完成地址块的hover效果
+// import '../../lib/jquery-2.1.4.js';
 
 //完成头部导航条hover效果
-    $('.ddnewhead_area').mouseenter(function () {
-        $(this).children().eq(0)[0].classList.add('hover');
-        $(this).children().eq(1).css('display', 'block');
-    })
-    $('.ddnewhead_area').mouseleave(function () {
-        $(this).children().eq(0)[0].classList.remove('hover');
-        $(this).children().eq(1).css('display', 'none');
-    })
+$('.menu_box').mouseenter(function () {
+    $(this).children().eq(0)[0].classList.add('hover');
+    $(this).children().eq(1).css('display', 'block');
+})
+$('.menu_box').mouseleave(function () {
+    $(this).children().eq(0)[0].classList.remove('hover');
+    $(this).children().eq(1).css('display', 'none');
+})
 
-}
+
+// 完成加载省份名称的加载渲染
+$.get('../data/address.json', '', function (data) {
+
+    $.each(data, function (index, item) {
+        var newLi = document.createElement('li');
+        newLi.innerHTML = '<a href="">' + item.province + '</a>';
+        $('.address-box').append(newLi);
+    })
+}, 'json')
+
+// 完成地址块的hover效果
+
+//完成头部导航条hover效果
+$('.ddnewhead_area').mouseenter(function () {
+    $(this).children().eq(0)[0].classList.add('hover');
+    $(this).children().eq(1).css('display', 'block');
+})
+$('.ddnewhead_area').mouseleave(function () {
+    $(this).children().eq(0)[0].classList.remove('hover');
+    $(this).children().eq(1).css('display', 'none');
+})
+
